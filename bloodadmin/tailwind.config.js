e0a06d39f07416e25019a2a949a7de89847ac064
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-05-24 05:35:15 +0100

    Costmise color

--- bloodadmin/tailwind.config.js
+++ bloodadmin/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+      'white': '#ffffff',
+      'primary_col': '#17173A',
+      'second_col': '#FB3640',
+      'second_col_ho': '#FB3660',
+      'third_col': '#FFD6E8',
+
+    }, 
+  },
   },
   plugins: [],
 }
