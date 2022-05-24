0bccf72135b03811737565ad2a74170d5c740250
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-05-24 05:40:59 +0100

    add views user

---
+++ bloodadmin/src/views/user/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Header/>
+        <MainHome/>
+        <FsHome/>
+        <SsHome/>
+        <Footer/>
+    </div>
+</template>
+
+<script >
+import Header from '../../components/user/Header.vue'
+import MainHome from '../../components/user/MainHome.vue'
+import FsHome from '../../components/user/FsHome.vue'
+import SsHome from '../../components/user/SsHome.vue'
+import Footer from '../../components/user/Footer.vue'
+export default {
+    name: 'Layout',
+    components: {
+        Header,
+        MainHome,
+        FsHome,
+        SsHome,
+        Footer
+    }
+}
+
+</script>
+
+<style  scoped>
+
+</style>
