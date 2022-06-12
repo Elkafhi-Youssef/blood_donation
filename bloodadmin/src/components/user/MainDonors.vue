0347db088b877a1c63fa35c513886c3a741163a0
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-06-12 02:49:42 +0100

    add name of function in form

--- bloodadmin/src/components/user/MainDonors.vue
+++ bloodadmin/src/components/user/MainDonors.vue
@@ -13,7 +13,7 @@
     <div
       class=" mt-6 mb-6 w-[80%] mx-auto px-2 py-2 bg-white border-2 border-red-400 rounded-lg"
     >
-      <form action="">
+      <form @submit.prevent="searchDonor" action="">
         <div class="flex md:justify-between flex-col sm:flex-col md:flex-row">
           <div class="flex flex-row border-b border-black w-full mx-2 lg:mx-4">
             <div>
@@ -93,10 +93,12 @@ export default {
         city: this.selecTCity,
         blood: this.selectBlood,
       };
+      
       await this.$store.dispatch('searchDonor',data)
       this.toDonors();
   },
   toDonors() {
+   
     this.$router.push('/donors');
   },
   },
