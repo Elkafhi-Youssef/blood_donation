378d5d3293164f1552876274e98d75abd6b01410
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-06-13 01:28:48 +0100

    add all function for request model

--- backendAPI/application/models/Requests.php
+++ backendAPI/application/models/Requests.php
@@ -13,5 +13,33 @@
         return $this->db->execute();
         
     }
-       
-     }
+    public function getNewRequest($id){
+        $this->db->prepareQuery("SELECT requests.request_date,requests.request_time,requests.description ,hospitals.hospital_name,hospitals.hospital_city,users.fullname,bloods.blood_type,requests.request_id,requests.request_status FROM bloods JOIN `users` ON bloods.blood_id = users.blood_id JOIN requests on users.user_id = requests.patient_id JOIN hospitals ON requests.hospital_id = hospitals.hospital_id WHERE requests.donor_id = ? and requests.request_status = 'new' ");
+        $this->db->bindValues([$id]);
+        $this->db->execute();
+        return $this->db->getResult();
+    }
+    public function getAppRequest($id){
+        $this->db->prepareQuery("SELECT requests.request_date,requests.request_time,requests.description ,hospitals.hospital_name,hospitals.hospital_city,users.fullname,bloods.blood_type,requests.request_id,requests.request_status FROM bloods JOIN `users` ON bloods.blood_id = users.blood_id JOIN requests on users.user_id = requests.patient_id JOIN hospitals ON requests.hospital_id = hospitals.hospital_id WHERE requests.donor_id = ? and requests.request_status = 'appointment' ");
+        $this->db->bindValues([$id]);
+        $this->db->execute();
+        return $this->db->getResult();
+    }
+    public function getCancelRequest($id){
+        $this->db->prepareQuery("SELECT requests.request_date,requests.request_time,requests.description ,hospitals.hospital_name,hospitals.hospital_city,users.fullname,bloods.blood_type,requests.request_id,requests.request_status FROM bloods JOIN `users` ON bloods.blood_id = users.blood_id JOIN requests on users.user_id = requests.patient_id JOIN hospitals ON requests.hospital_id = hospitals.hospital_id WHERE requests.donor_id = ? and requests.request_status = 'canceled' ");
+        $this->db->bindValues([$id]);
+        $this->db->execute();
+        return $this->db->getResult();
+    }
+    public function acceptRequest($id){
+        $this->db->prepareQuery("UPDATE `requests` SET `request_status` = 'appointment' WHERE `requests`.`request_id` = ? ");
+        $this->db->bindValues([$id]);
+        return $this->db->execute();
+    }
+    public function cancelRequest($id){
+        $this->db->prepareQuery("UPDATE `requests` SET `request_status` = 'canceled' WHERE `requests`.`request_id` = ? ");
+        $this->db->bindValues([$id]);
+        return $this->db->execute();
+    }
+
+    }
