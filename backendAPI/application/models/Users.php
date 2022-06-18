1cff8ea8fa7901cca9db0be3964ee34e8b7a2519
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-06-18 22:08:18 +0100

    function for admin in model user

--- backendAPI/application/models/Users.php
+++ backendAPI/application/models/Users.php
@@ -2,27 +2,32 @@
 
     class Users{
 
-        private $db = null;
-        public function __construct(){
-            $this->db = new Db();
-        }
-        public function createUser($fullname,$phone,$email,$city,$password,$role,$blood_id,$age,$image){
-            $this->db->prepareQuery("INSERT INTO `users` (`user_id`, `fullname`, `phone`, `email`, `city`, `password`, `role`, `blood_id`, `age`, `image`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?,?)");
-            $this->db->bindValues([$fullname,$phone,$email,$city,$password,$role,$blood_id,$age,$image]);
-            return $this->db->execute();
-            
-        }
-        public function updateProfile($fullname,$phone,$email,$city,$role,$blood_id,$age,$image,$id){
-            $this->db->prepareQuery("UPDATE `users` SET `fullname` = ?, `phone` = ?, `email` = ?, `city` = ?, `role` = ?, `blood_id` = ?, `age` = ?, `image` = ? WHERE `users`.`user_id` =?");
-            $this->db->bindValues([$fullname,$phone,$email,$city,$role,$blood_id,$age,$image,$id]);
-            return $this->db->execute();
-            
-        }
-        public function login($email){
-            $this->db->prepareQuery("SELECT * FROM users where email = ?");
-            $this->db->execute([$email]);
-            return $this->db->getRow(); 
-        }
+    private $db = null;
+    public function __construct(){
+        $this->db = new Db();
+    }
+    public function createUser($fullname,$phone,$email,$city,$password,$role,$blood_id,$age,$image){
+        $this->db->prepareQuery("INSERT INTO `users` (`user_id`, `fullname`, `phone`, `email`, `city`, `password`, `role`, `blood_id`, `age`, `image`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?,?)");
+        $this->db->bindValues([$fullname,$phone,$email,$city,$password,$role,$blood_id,$age,$image]);
+        return $this->db->execute();
+        
+    }
+    public function updateProfile($fullname,$phone,$email,$city,$role,$blood_id,$age,$image,$id){
+        $this->db->prepareQuery("UPDATE `users` SET `fullname` = ?, `phone` = ?, `email` = ?, `city` = ?, `role` = ?, `blood_id` = ?, `age` = ?, `image` = ? WHERE `users`.`user_id` =?");
+        $this->db->bindValues([$fullname,$phone,$email,$city,$role,$blood_id,$age,$image,$id]);
+        return $this->db->execute();
+        
+    }
+    public function login($email){
+        $this->db->prepareQuery("SELECT * FROM users where email = ?");
+        $this->db->execute([$email]);
+        return $this->db->getRow(); 
+    }
+    public function loginAdmin($email){
+        $this->db->prepareQuery("SELECT * FROM admins where email = ?");
+        $this->db->execute([$email]);
+        return $this->db->getRow(); 
+    }
     public function getusersshome(){
         $this->db->prepareQuery("SELECT user_id , fullname ,city,times,image,blood_type FROM users join bloods on users.blood_id = bloods.blood_id where users.role='donor' order by times desc limit 8");
         $this->db->execute();
@@ -63,11 +68,25 @@
             return $this->db->getResult();
         }
     }
-    public function getAllUser(){
+    public function getAllDonor(){
         $this->db->prepareQuery("SELECT * FROM users join bloods on users.blood_id = bloods.blood_id and users.role = 'donor' ");
         $this->db->execute();
         return $this->db->getResult();
     }
-       
-    
+    public function getAllUsersAdmin(){
+        $this->db->prepareQuery("SELECT * FROM users join bloods on users.blood_id = bloods.blood_id  ");
+        $this->db->execute();
+        return $this->db->getResult();
+    }
+    public function updateUserByAdmin($fullname,$phone,$email,$city,$role,$blood_id,$age,$id){
+        $this->db->prepareQuery("UPDATE `users` SET `fullname` = ?, `phone` = ?, `email` = ?, `city` = ?, `role` = ?, `blood_id` = ?, `age` = ? WHERE `users`.`user_id` =?");
+        $this->db->bindValues([$fullname,$phone,$email,$city,$role,$blood_id,$age,$id]);
+        return $this->db->execute();
+        
+    }
+    public function deleteUserByAdmin($id){
+        $this->db->prepareQuery("DELETE FROM `users` WHERE `users`.`user_id` = ?");
+        $this->db->bindValues([$id]);
+        return $this->db->execute();
+    }
  }
