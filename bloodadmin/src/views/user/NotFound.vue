8b6cf7847562447314ba5c2448ba052311108439
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-06-18 22:11:48 +0100

    add not found page

---
+++ bloodadmin/src/views/user/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=" text-center">
+        <h1>Page Not Found</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound",
+    }
+</script>
+
+<style scoped>
+
+</style>
