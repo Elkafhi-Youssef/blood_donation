56717800e1bfd58096dc4fac18d26547e1350fd0
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-06-18 22:05:21 +0100

    function get hospitals and new hospitals

--- backendAPI/application/controllers/Hospital.php
+++ backendAPI/application/controllers/Hospital.php
@@ -28,6 +28,41 @@ class Hospital extends Controller
         }
         $this->response();
     }
+    public function getAllHospitalsAdmin(){
+        $hospitals = $this->modelInstance->getAllHospitalsAdmin();
+        if($hospitals){
+            $this->res['data'] = $hospitals;
+            $this->res['code'] = 200;
+            $this->res['message'] = "success";
+        }else{
+            $this->res['code'] = 200;
+            $this->res['message'] = "error";
+        }
+        $this->response();
+    }
+    function addHospitalAdmin(){
+        if(($_SERVER['REQUEST_METHOD'] == 'POST')){
+            $data = json_decode(file_get_contents("php://input"));
+            $hospitalCity = $data->data->city;
+            $hospitaladdress = $data->data->address;
+            $hospitalname = $data->data->name;
+            $check = $this->modelInstance->addHospitalAdmin($hospitalname,$hospitalCity,$hospitaladdress);
+           
+            if($check){
+                $this->res['alert'] = "Hospotal added successfully";
+                $this->res['code'] = 200;
+                $this->res['message'] = "success";
+            }
+            else{
+                $this->res['data'] = "No data";
+                $this->res['alert'] = "Hospital didn't add successfully";
+                $this->res['message'] = "failed";
+                $this->res['code'] = 404;
+            }
+            
+        }
+        $this->response();
+    }
    
  
     
